6fc396639bfe3eb1a0a9695fc6a764747fb0e152
Raine-gay <122524546+Raine-gay@users.noreply.github.com>
2023-07-09 20:47:14 +0000

    Update vscode.nix

--- vscode/vscode.nix
+++ vscode/vscode.nix
@@ -4,5 +4,9 @@
   programs.vscode = {
     enable = true; # Enable vscode
     package = pkgs.vscodium; # Switch to vscodium.
+
+    userSettings = {
+      "workbench.editor.labelFormat" = "short";
+    };
   };
 }
