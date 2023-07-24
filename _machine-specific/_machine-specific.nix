ce55f92ca44e5c9013abce28385f39804435cc65
na <na@na.com>
2023-07-24 22:21:23 +0100

    Updated comments

--- _machine-specific/_machine-specific.nix
+++ _machine-specific/_machine-specific.nix
@@ -1,5 +1,8 @@
 { config, ... }:
 
+# Any changes to the _machine-specific/ directory is untracked by git.
+# If this is not the case use the following command in the root folder: git update-index --skip-worktree _machine-specific/*
+
 {
   imports = [ # Place your machine specific imports here.
     
