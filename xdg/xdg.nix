9688c4ce510397272f6798851ae5487dfc8a8284
NA <NA@NA.com>
2023-07-26 12:56:03 +0100

    Added flatpack to xdg data dirs

--- xdg/xdg.nix
+++ xdg/xdg.nix
@@ -3,6 +3,6 @@
 {
   xdg = {
     enable = true; # Allow home manager to control xdg base directories.
-    systemDirs.data = [ "/usr/share" "/usr/local/share" "$HOME/.nix-profile/share" ];
+    systemDirs.data = [ "/usr/share" "/usr/local/share" "$HOME/.nix-profile/share" "/var/lib/flatpak/exports/share" ];
   };
 }
