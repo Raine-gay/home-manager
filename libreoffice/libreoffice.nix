ee18db50839668b9ccc4e7586f5e4a5c326694b8
NA <NA@NA.com>
2023-07-29 13:11:59 +0100

    Added hunspell dictionary

--- libreoffice/libreoffice.nix
+++ libreoffice/libreoffice.nix
@@ -4,5 +4,6 @@
   home.packages = [
     pkgs.libreoffice # Enables libreoffice.
     pkgs.hunspell # Enables hunspell for spell checking
+    pkgs.hunspellDicts.en_GB-large # Add the en_GB hunspell dictionary
   ];
 }
