4a6ed11d3577de71de4531e14aaefd96ef4742c9
NA <NA@NA.com>
2023-08-06 13:19:41 +0100

    added extensions to vs code

--- visual-studio-code/visual-studio-code.nix
+++ visual-studio-code/visual-studio-code.nix
@@ -3,5 +3,15 @@
 {
   programs.vscode = {
     enable = true; # Enables visual-studio code.
+
+    extensions = [
+      pkgs.vscode-extensions.rust-lang.rust-analyzer # Add rust anal to visual code.
+      pkgs.vscode-extensions.jnoortheen.nix-ide # Add the nix ide to visual code.
+      pkgs.vscode-extensions.vadimcn.vscode-lldb # lldb debugger.
+      pkgs.vscode-extensions.tamasfe.even-better-toml # Even better toml support.
+      pkgs.vscode-extensions.donjayamanne.githistory # Git history viewer
+      pkgs.vscode-extensions.streetsidesoftware.code-spell-checker # Code spell checker.
+      pkgs.vscode-extensions.usernamehw.errorlens # Error lens for inline errors.
+    ]
   };
 }
