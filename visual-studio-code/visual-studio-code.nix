a7018c1548b08c5ad28f8fb532bebb3f2f8ae73a
NA <NA@NA.com>
2024-02-12 16:50:27 +0000

    Yeeted lldb

--- visual-studio-code/visual-studio-code.nix
+++ visual-studio-code/visual-studio-code.nix
@@ -7,7 +7,6 @@
     extensions = [
       pkgs.vscode-extensions.rust-lang.rust-analyzer # Add rust anal to visual code.
       pkgs.vscode-extensions.jnoortheen.nix-ide # Add the nix ide to visual code.
-      pkgs.vscode-extensions.vadimcn.vscode-lldb # lldb debugger.
       pkgs.vscode-extensions.tamasfe.even-better-toml # Even better toml support.
       pkgs.vscode-extensions.donjayamanne.githistory # Git history viewer
       pkgs.vscode-extensions.streetsidesoftware.code-spell-checker # Code spell checker.
